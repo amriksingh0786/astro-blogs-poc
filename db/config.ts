7b098572166ac199f64a2c815a1b0c34263e6fc1
amrik <Amrik.Khalsa@cyncly.com>
2024-05-29 00:54:54 +0530

    Done with basic setup of DB and Blogs,  Refactor database configuration and seed script

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,60 @@
-import { defineDb } from 'astro:db';
+/*  import { defineDb, defineTable, column } from "astro:db";
 
 // https://astro.build/db/config
+
+const Comment = defineTable({
+	columns: {
+	  author: column.text(),
+	  body: column.text(),
+	},
+  });
+
+const Blogs = defineTable({
+  columns: {
+    title: column.text(),
+    blogBody: column.text(),
+    commentAuthor: column.text(),
+    commentBody: column.text(),
+  },
+  foreignKeys: [
+    {
+      columns: ["commentAuthor", "commentBody"],
+      references: () => [Comment.columns.author, Comment.columns.body],
+    },
+  ],
+});
+
+
 export default defineDb({
-	tables: {},
+  tables: { Comment, Blogs },
+});
+ 
+
+ */
+
+import { defineDb, defineTable, column } from "astro:db";
+
+// Define the Comment table
+const Comment = defineTable({
+  columns: {
+	id: column.number({ primaryKey: true }),
+    author: column.text(),
+    body: column.text(),
+  },
+});
+
+// Define the Blogs table
+const Blogs = defineTable({
+  deprecated: true,
+  columns: {
+	id: column.number({ primaryKey: true }),
+    title: column.text(),
+    blogBody: column.text(),
+	commentId: column.number({ references: () => Comment.columns.id }),
+  },
+});
+
+// Export the database definition
+export default defineDb({
+  tables: { Comment, Blogs },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,33 @@
-import { db } from 'astro:db';
+import { db, Comment, Blogs } from "astro:db";
 
 export default async function seed() {
-	// Seed local development data.
-	// See https://docs.astro.build/en/guides/astro-db/#seed-your-database
+  console.log("Seeding data");
+  console.log("db", db);
+  await db.insert(Comment).values([
+    { id: 1, author: "amrik", body: "Hope you like Astro DB!" },
+    { id: 2, author: "aman", body: "Enjoy!" },
+  ]);
+  await db.insert(Blogs).values([
+    {
+      id: 1,
+      title: "First Blog",
+      blogBody: "This is my first blog",
+      commentId: 1,
+      /*  commentAuthor: "amrik",
+      commentBody: "Hope you like Astro DB!", */
+    },
+    {
+      id: 2,
+      title: "Second Blog",
+      blogBody: "This is my second blog",
+      commentId: 2,
+      /*       commentAuthor: "aman",
+      commentBody: "Enjoy!", */
+    },
+  ]);
+
+  //   console.log("db", Comment);
+
+  // Seed local development data.
+  // See https://docs.astro.build/en/guides/astro-db/#seed-your-database
 }

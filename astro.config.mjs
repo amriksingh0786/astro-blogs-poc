eddd50fd9b4c2fcdb23922acdd9f4026cbe48d44
amrik <Amrik.Khalsa@cyncly.com>
2024-05-29 03:03:16 +0530

    added adapters for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'server', // 'hybrid' | 'svelte' | 'react' | 'vue' | 'solid
-	integrations: [db()],
+  output: 'server',
+  // 'hybrid' | 'svelte' | 'react' | 'vue' | 'solid
+  integrations: [db()],
+  adapter: vercel()
 });
